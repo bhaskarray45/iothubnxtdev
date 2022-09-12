9e2745869b64e72eeebb1d4068975848bceefebe
bhaskarray45 <bhaskar.ray45@gmail.com>
2022-09-12 13:32:14 +0530

    Initial commit

---
+++ src/main/java/com/example/iot/demo/azureiotdemo/constants/ApplicationConstant.java
@@ -0,0 +1,17 @@
+package com.example.iot.demo.azureiotdemo.constants;
+
+
+public class ApplicationConstant {
+    
+    public static final String IOT_HUB_CONN_STRING = "HostName=iothubnxt.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=55IDDSZYg8fBXhk+mpIvWYjBP20e1wWnerVCmxIDPN8=";
+
+    public static final String IOT_HUB_CONN_STRING_1 = "HostName=iothubnxt.azure-devices.net;DeviceId=iot-hub-1;SharedAccessKey=4F/ElnEgAve/R3pBcjOQYQ==";
+
+    public static final String DEVICE_ID = "iot-hub-1";
+
+    public static final String PROTOCOL = "mqtt";
+
+    public static final int D2C_MESSAGE_TIMEOUT = 120;
+
+
+}
---
+++ src/test/java/com/example/iot/demo/azureiotdemo/AzureIotDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iot.demo.azureiotdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureIotDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
